c9396f598d4a6bf7f2dcf3575c220448654681ac
Joe Yates <joe.g.yates@gmail.com>
2026-04-15 09:45:41 +0200

    Run non-app commands

---
+++ lib/dokku_remote/command.ex
@@ -0,0 +1,31 @@
+defmodule DokkuRemote.Command do
+  @system_impl Application.compile_env(:dokku_remote, :System, System)
+
+  @callback run(dokku_host :: String.t(), command :: String.t()) ::
+              {:ok, String.t()} | {:error, String.t(), non_neg_integer()}
+
+  def run(dokku_host, command, opts \\ []) do
+    verbose = Keyword.get(opts, :verbose, false)
+
+    into =
+      if verbose do
+        CollectableStreamer.new(fn line -> IO.write(line) end)
+      else
+        ""
+      end
+
+    full_command = "ssh dokku@#{dokku_host} #{command} 2>&1"
+
+    if verbose do
+      IO.puts("Running command: #{full_command}")
+    end
+
+    case @system_impl.shell(full_command, into: into) do
+      {output, 0} ->
+        {:ok, output}
+
+      {output, exit} ->
+        {:error, output, exit}
+    end
+  end
+end
