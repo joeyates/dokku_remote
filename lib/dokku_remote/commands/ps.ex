cfce306c24e0b7d4424f21d65c15866dfb80734e
Joe Yates <joe.g.yates@gmail.com>
2026-04-16 15:04:31 +0200

    DokkuRemote.Commands.Ps: add report/1 and @callbacks

--- lib/dokku_remote/commands/ps.ex
+++ lib/dokku_remote/commands/ps.ex
@@ -6,6 +6,65 @@ defmodule DokkuRemote.Commands.Ps do
                 )
 
   @type scales :: %{String.t() => __MODULE__.Scale.t()}
+  @type entry :: %{
+          app: String.t(),
+          process_type: String.t(),
+          process_index: non_neg_integer(),
+          state: String.t(),
+          cid: String.t()
+        }
+
+  @callback report(String.t()) :: {:ok, [entry()]} | {:error, any(), any()}
+  @callback scale(String.t()) :: {:ok, scales()} | {:error, any(), any()}
+  @callback scale(String.t(), String.t()) :: {:ok, __MODULE__.Scale.t()} | {:error, any(), any()}
+
+  @spec report(String.t()) :: {:ok, [entry()]} | {:error, any(), any()}
+  def report(dokku_host) do
+    case @command_impl.run(dokku_host, "ps:report") do
+      {:ok, output} -> {:ok, parse_report(output)}
+      {:error, output, exit_code} -> {:error, output, exit_code}
+    end
+  end
+
+  defp parse_report(output) do
+    {entries, _current_app} =
+      output
+      |> String.split("\n", trim: true)
+      |> Enum.reduce({[], nil}, fn line, {acc, current_app} ->
+        cond do
+          String.starts_with?(line, "=====>") ->
+            app = line |> String.replace_prefix("=====>", "") |> String.split() |> List.first()
+            {acc, app}
+
+          current_app != nil ->
+            case parse_status_line(line) do
+              {:ok, entry} -> {[Map.put(entry, :app, current_app) | acc], current_app}
+              :error -> {acc, current_app}
+            end
+
+          true ->
+            {acc, current_app}
+        end
+      end)
+
+    Enum.reverse(entries)
+  end
+
+  defp parse_status_line(line) do
+    case Regex.run(~r/Status (\w+) (\d+):\s+(\w+) \(CID: ([a-f0-9]+)\)/, line) do
+      [_, process_type, index_str, state, cid] ->
+        {:ok,
+         %{
+           process_type: process_type,
+           process_index: String.to_integer(index_str),
+           state: state,
+           cid: cid
+         }}
+
+      _ ->
+        :error
+    end
+  end
 
   @spec scale(String.t()) :: {:ok, scales()} | {:error, any(), any()}
   def scale(dokku_host) do
