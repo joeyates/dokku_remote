695829a82d5f23525b1d8a743d31a7e3e6606e2c
Joe Yates <joe.g.yates@gmail.com>
2026-04-23 15:42:36 +0200

    Add doc and type

--- lib/dokku_remote/app.ex
+++ lib/dokku_remote/app.ex
@@ -1,7 +1,16 @@
 defmodule DokkuRemote.App do
+  @moduledoc """
+  Represents a Dokku application and its connection details.
+  """
   @required_keys [:dokku_app, :dokku_host]
   @enforce_keys @required_keys
   defstruct @required_keys ++ [verbose: false]
+
+  @type t :: %__MODULE__{
+          dokku_app: String.t(),
+          dokku_host: String.t(),
+          verbose: boolean()
+        }
 
   def new(opts) do
     struct!(__MODULE__, opts)
